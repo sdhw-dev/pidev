f010ac5d02cef97eb8ecea5c76f3b34d05d0c7bd
sdhw-dev <wissal.sdh@gmail.com>
2021-03-14 19:18:28 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
 import Services from './components/pages/Services';
 import Products from './components/pages/Products';
-import SignUp from './components/pages/SignUp'; 
+import SignUp from './components/pages/SignUp';
+import Connexion from './components/pages/Connexion';  
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route path='/services' component={Services} />
           <Route path='/products' component={Products} />
           <Route path='/sign-up' component={SignUp} />
+          <Route path='/connexion' component={Connexion} />
         </Switch>
       </Router>
     </>
---
+++ src/components/pages/Connexion.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import '../../App.css';
+
+export default function Connexion() {
+  return <h1 className='connexion'></h1>;
+}
