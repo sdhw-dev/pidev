dd5ebd3a888d12493d9b84229f1e4c8cee5c2b05
EmmaTah <oumaimatahiri71@gmail.com>
2021-04-20 03:19:04 +0000

    oumaima 24

--- src/components/pages/EventCalendar.js
+++ src/components/pages/EventCalendar.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import NavbarServices from "../NavbarServices";
+import Sidebar from '../SideBar'
+import "./calendar.css";
 
 export default class EventCalendar extends React.Component {
 
     render() {
+        
         return (
+           
+            <div >
+                <NavbarServices/>
+                <div style= {{display:'flex' , flexDirection :'row'}} >
+               <Sidebar/>
           <FullCalendar
             plugins={[ dayGridPlugin ]}
             eventContent={renderEventContent}
@@ -14,6 +23,8 @@ export default class EventCalendar extends React.Component {
               { title: 'Troc 2', date: '2021-04-15' }
             ]}
           />
+          </div>
+            </div>
         )
       }
     }
