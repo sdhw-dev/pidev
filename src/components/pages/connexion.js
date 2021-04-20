09cdc3fe4263959149b6073b5592178d97f071be
Yassine Naciri <yassinen84@gmail.com>
2021-04-20 15:24:23 +0000

    changement lien base de donné

--- src/components/pages/connexion.js
+++ src/components/pages/connexion.js
@@ -33,97 +33,96 @@ class Connexion extends Component {
   render() {
     return (
       <>
-      <div className="connexion">
-        <Container maxWidth="sm" style={{}}>
-          <Box
-            border={1}
-            display="flex"
-            flexDirection="column"
-            style={{ borderRadius: 15, borderColor: "#CACACA" }}
-          >
-            <header className="header">
-              <Route
-                render={({ history }) => (
-                  <button
-                    className="btn"
-                    key="inscription"
-                    style={{ backgroundColor: "#008000", marginLeft: "auto" }}
-                    onClick={() => {
-                      history.push("/sign-up");
-                    }}
-                  >
-                    Créer un compte
-                  </button>
-                )}
-              />
-            </header>
-            <Box display="flex" flexDirection="column">
-              <TextField
-                id="Mail"
-                label="Adresse e-Mail"
-                style={{ margin: 8 }}
-                margin="normal"
-                variant="outlined"
-                error={this.state.erreur}
-                onChange={(event) => {
-                  this.state.infoAuthentification.mail = event.target.value;
-                }}
-              />
-              <TextField
-                id="Password"
-                label="Mot de passe"
-                style={{ margin: 8 }}
-                margin="normal"
-                type="password"
-                variant="outlined"
-                error={this.state.erreur}
-                helperText={
-                  this.state.erreur
-                    ? "adresse mail ou mot de passe incorrect "
-                    : ""
-                }
-                onChange={(event) => {
-                  this.state.infoAuthentification.pass = event.target.value;
-                }}
-              />
+        <div className="connexion">
+          <Container maxWidth="sm" style={{}}>
+            <Box
+              border={1}
+              display="flex"
+              flexDirection="column"
+              style={{ borderRadius: 15, borderColor: "#CACACA" }}
+            >
+              <header className="header">
+                <Route
+                  render={({ history }) => (
+                    <button
+                      className="btn"
+                      key="inscription"
+                      style={{ backgroundColor: "#008000", marginLeft: "auto" }}
+                      onClick={() => {
+                        history.push("/sign-up");
+                      }}
+                    >
+                      Créer un compte
+                    </button>
+                  )}
+                />
+              </header>
+              <Box display="flex" flexDirection="column">
+                <TextField
+                  id="Mail"
+                  label="Adresse e-Mail"
+                  style={{ margin: 8 }}
+                  margin="normal"
+                  variant="outlined"
+                  error={this.state.erreur}
+                  onChange={(event) => {
+                    this.state.infoAuthentification.mail = event.target.value;
+                  }}
+                />
+                <TextField
+                  id="Password"
+                  label="Mot de passe"
+                  style={{ margin: 8 }}
+                  margin="normal"
+                  type="password"
+                  variant="outlined"
+                  error={this.state.erreur}
+                  helperText={
+                    this.state.erreur
+                      ? "adresse mail ou mot de passe incorrect "
+                      : ""
+                  }
+                  onChange={(event) => {
+                    this.state.infoAuthentification.pass = event.target.value;
+                  }}
+                />
+              </Box>
+              <footer>
+                <Route
+                  render={({ history }) => (
+                    <button
+                      className="btn"
+                      key="connection"
+                      style={{
+                        backgroundColor: "#008000",
+                        color: "#000000",
+                        borderColor: "#000000",
+                      }}
+                      onClick={async () => {
+                        await handleConnection(
+                          this.state.infoAuthentification
+                        ).then((res) => {
+                          if (!res) {
+                            this.setState({ erreur: true });
+                          } else {
+                            this.setState({ erreur: false });
+                            console.log(res);
+                            this.props.onConnexion(res);
+                            history.push("./EspaceP");
+                          }
+                        });
+                      }}
+                    >
+                      {" "}
+                      Se connecter{" "}
+                    </button>
+                  )}
+                />
+              </footer>
             </Box>
-            <footer>
-              <Route
-                render={({ history }) => (
-                  <button
-                    className="btn"
-                    key="connection"
-                    style={{
-                      backgroundColor: "#008000",
-                      color: "#000000",
-                      borderColor: "#000000",
-                    }}
-                    onClick={async () => {
-                      await handleConnection(
-                        this.state.infoAuthentification
-                      ).then((res) => {
-                        if (!res) {
-                          this.setState({ erreur: true });
-                        } else {
-                          this.setState({ erreur: false });
-                          console.log(res);
-                          this.props.onConnexion(res);
-                          history.push("./EspaceP");
-                        }
-                      });
-                    }}
-                  >
-                    {" "}
-                    Se connecter{" "}
-                  </button>
-                )}
-              />
-            </footer>
-          </Box>
-        </Container>
-        
-      </div>
-      <Footer/>
+          </Container>
+        </div>
+        <Footer />
       </>
     );
   }
