f9086517db14b867ce0a3c1a720a3fd34f4ef64d
EmmaTah <oumaimatahiri71@gmail.com>
2021-04-27 15:02:10 +0000

    Oumaima 110

--- backEnd/models/annonceModel.js
+++ backEnd/models/annonceModel.js
@@ -5,7 +5,7 @@ mongoose.Promise = global.Promise;
 /*mongodb+srv://yassine:yassinenaciri@cluster0.hm988.mongodb.net/app?retryWrites=true&w=majority*/
 mongoose
   .connect(
-    "mongodb+srv://yassine:yassinenaciri@cluster0.hm988.mongodb.net/app?retryWrites=true&w=majority",
+    "mongodb+srv://oumaima:oumaimatahiri@cluster0.hm988.mongodb.net/app?retryWrites=true&w=majority",
     { useNewUrlParser: true }
   )
   .then(() => {
